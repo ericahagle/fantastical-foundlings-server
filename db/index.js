9fb27434dc121584475c84186042bbbc31fba3e6
Erica Hagle <ericahagle.dev@gmail.com>
2024-02-27 08:31:14 -0800

    fix: Correct typo for mongodb port

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://127.0.0.1:2707/staff', { useNewUrlParser: true })
+  .connect('mongodb://127.0.0.1:27017/staff', { useNewUrlParser: true })
   .catch(error => {
     console.error('Connection error', error.message);
   });
